42624a4ad6de0ad331ef0b281afa02328be656bd
habib-SE <hab67603@gmail.com>
2023-12-07 00:10:38 +0500

    create form using formik library in react

--- formik-react/src/App.js
+++ formik-react/src/App.js
@@ -1,8 +1,37 @@
+import './App.css'
+import { useFormik } from 'formik';
 
 function App() {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: ''
+    }
+  })
+console.log("fomik submitt", formik.values)
   return (
-    <div className="App">
-     
+    <div className="App container items-center justify-items-center py-7 ml-11" >
+     <form className=' flex flex-col w-[30%] border border-l-4 gap-5'>
+      <label className=' pt-3'>Your name:</label>
+      <input
+      className='border-2 rounded-md bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6'
+      type='text'
+      name='name'
+      placeholder='enter your name'
+      onChange={formik.handleChange}
+      value={formik.values.name}
+      />
+      <label className=' pt-3'>Your Email</label>
+      <input
+      className='border-2 rounded-md bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6'
+      type='email'
+      name='email'
+      placeholder='enter your email'
+      onChange={formik.handleChange}
+      value={formik.values.email}
+      />
+      <button className=' py-2 px-5 text-white bg-black border'>Submitt</button>
+     </form>
     </div>
   );
 }
